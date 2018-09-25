ccfa1de8e5ec069162bad459822d19bb5f218ab4
sendtion <sendtion@163.com>
2018-09-25 14:10:44 +0800

    静态代理和动态代理的练习

--- app/src/main/java/com/sdc/designpatterns/proxy/ProxyDemo.java
+++ app/src/main/java/com/sdc/designpatterns/proxy/ProxyDemo.java
@@ -29,25 +29,31 @@ public class ProxyDemo {
         //动态代理
         MaoTaiJiu maoTaiJiu = new MaoTaiJiu();
         WuLiangYe wuLiangYe = new WuLiangYe();
-
         FuRongWang fuRongWang = new FuRongWang();
 
         InvocationHandler handler1 = new GuiTaiA(maoTaiJiu);
         InvocationHandler handler2 = new GuiTaiA(wuLiangYe);
-
         InvocationHandler handler3 = new GuiTaiA(fuRongWang);
 
-        SellWine sellWine1 = (SellWine) Proxy.newProxyInstance(MaoTaiJiu.class.getClassLoader(),
+        SellWine dynamicProxy1 = (SellWine) Proxy.newProxyInstance(MaoTaiJiu.class.getClassLoader(),
                 MaoTaiJiu.class.getInterfaces(), handler1);
-        SellWine sellWine2 = (SellWine) Proxy.newProxyInstance(MaoTaiJiu.class.getClassLoader(),
+        SellWine dynamicProxy2 = (SellWine) Proxy.newProxyInstance(MaoTaiJiu.class.getClassLoader(),
                 MaoTaiJiu.class.getInterfaces(), handler2);
-        SellCigarette sellCigarette = (SellCigarette) Proxy.newProxyInstance(FuRongWang.class.getClassLoader(),
+        SellCigarette dynamicProxy3 = (SellCigarette) Proxy.newProxyInstance(FuRongWang.class.getClassLoader(),
                 FuRongWang.class.getInterfaces(), handler3);
 
-        sellWine1.maiJiu();
+        dynamicProxy1.maiJiu();
         System.out.println("******************************");
-        sellWine2.maiJiu();
+        dynamicProxy2.maiJiu();
         System.out.println("******************************");
-        sellCigarette.maiYan();
+        dynamicProxy3.maiYan();
+
+        System.out.println("******************************");
+
+        //动态生成的 proxy class 与 Proxy 这个类同一个包。
+        //动态生成的代理类的名字是不是包名+$Proxy+id序号。
+        System.out.println("dynamicProxy1 class name : " + dynamicProxy1.getClass().getName());
+        System.out.println("dynamicProxy2 class name : " + dynamicProxy2.getClass().getName());
+        System.out.println("dynamicProxy3 class name : " + dynamicProxy3.getClass().getName());
     }
 }
